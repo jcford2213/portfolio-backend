218d7415d1d42b9ee09e981269e19f24f3f6583c
IdioticMarlin22 <jcrantfo@comcast.net>
2023-03-31 10:50:45 -0400

    Added a get request handle

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const sendEmail = require('../controllers/sendMail');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send("<p>Yo, Sup dawg</p>")
+});
+
 router.post('/', async (req, res) => {
   await sendEmail(req.body)
   console.log(req.body)
