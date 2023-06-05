231360cdd6f99a35d110d57ab5ddef93b4f61e5a
IdioticMarlin22 <jcrantfo@comcast.net>
2023-06-05 19:41:39 -0400

    Added CORS limitation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors(process.env.CLIENT_ENDPOINT));
 app.use(express.json());
 app.use(urlencoded({extended: true}));
 
